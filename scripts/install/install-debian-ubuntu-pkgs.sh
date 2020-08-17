048c8c518bc7a907be06278925257ba26034ad11
Matthew Jorgensen <matthew@jrgnsn.net>
2020-08-17 13:06:46 -0500

    Check if gotop is already installed.

--- scripts/install/install-debian-ubuntu-pkgs.sh
+++ scripts/install/install-debian-ubuntu-pkgs.sh
@@ -16,11 +16,15 @@ sudo apt install -y \
 	zsh
 
 echo "Installing gotop..."
-curl -s https://api.github.com/repos/xxxserxxx/gotop/releases/latest \
-| grep "browser_download_url.*linux_amd64" \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| xargs -I {} curl -L "{}" -o /tmp/gotop.tgz
-sudo tar xzvf /tmp/gotop.tgz -C /usr/bin/
+if [[ -x /usr/bin/gotop ]]; then
+	echo "gotop is already installed."
+else
+	curl -s https://api.github.com/repos/xxxserxxx/gotop/releases/latest \
+	| grep "browser_download_url.*linux_amd64" \
+	| cut -d : -f 2,3 \
+	| tr -d \" \
+	| xargs -I {} curl -L "{}" -o /tmp/gotop.tgz
+	sudo tar xzvf /tmp/gotop.tgz -C /usr/bin/
+fi
 
 confirm_n "Install Nginx?" && "$ROOTDIR/install/install-deb-nginx.sh"
