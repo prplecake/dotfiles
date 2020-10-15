5cc5f23d480caa254e2638621c50deab7b68c6fd
Matthew Jorgensen <matthew@jrgnsn.net>
2020-10-15 01:48:35 -0500

    Rename scripts/install to scripts/src
    (Makes autocompletion to install.sh much quicker!)

--- scripts/install.sh
+++ scripts/install.sh
@@ -7,25 +7,25 @@ source "$ROOTDIR/_helpers"
 # install tools
 if [[ "$OSTYPE" == linux-gnu* ]]; then
 	if [[ -x "/etc/debian_version" || -x "/etc/" ]]; then
-		confirm "Install Debian/Ubuntu packages?" && "$ROOTDIR/install/install-debian-ubuntu-pkgs.sh"
+		confirm "Install Debian/Ubuntu packages?" && "$ROOTDIR/src/install-debian-ubuntu-pkgs.sh"
 	fi
-	confirm "Set hostname?" 				&& "$ROOTDIR/install/install-set-hostname.sh"
-	confirm "Install node_exporter?"		&& "$ROOTDIR/install/install-node-exporter.sh"
+	confirm "Set hostname?" 				&& "$ROOTDIR/src/install-set-hostname.sh"
+	confirm "Install node_exporter?"		&& "$ROOTDIR/src/install-node-exporter.sh"
 elif [[ "$OSTYPE" == darwin* ]]; then
-	confirm "Install Homebrew?"				&& "$ROOTDIR/install/install-homebrew.sh"
-	confirm "Install Homebrew packages?"	&& "$ROOTDIR/install/install-homebrew-packages.sh"
-	confirm "Install Sublime {Text,Merge} CLI helpers?" && "$ROOTDIR/install/install-macos-subl-smerge-helpers.sh"
+	confirm "Install Homebrew?"				&& "$ROOTDIR/src/install-homebrew.sh"
+	confirm "Install Homebrew packages?"	&& "$ROOTDIR/src/install-homebrew-packages.sh"
+	confirm "Install Sublime {Text,Merge} CLI helpers?" && "$ROOTDIR/src/install-macos-subl-smerge-helpers.sh"
 fi
 
 # Extra steps
-confirm "Set login shell?"	&& "$ROOTDIR/install/install-shell.sh"
-confirm "Create symlinks?"	&& "$ROOTDIR/install/install-symlinks.sh"
+confirm "Set login shell?"	&& "$ROOTDIR/src/install-shell.sh"
+confirm "Create symlinks?"	&& "$ROOTDIR/src/install-symlinks.sh"
 
-if [[ -e "$ROOTDIR/install/set_hostname" ]]; then
+if [[ -e "$ROOTDIR/src/set_hostname" ]]; then
 	confirm "Hostname was changed. Reboot?" && reboot=1
 fi
 
 if [[ $reboot == 1 ]]; then
-	rm "$ROOTDIR/install/set_hostname"
+	rm "$ROOTDIR/src/set_hostname"
 	sudo reboot
 fi
