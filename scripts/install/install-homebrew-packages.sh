790b94f3ba897f53ec11cce2ef7ec0e4d4dc7dfc
Matthew Jorgensen <matthew@jrgnsn.net>
2020-10-15 01:47:07 -0500

    Remove Brewfile and add contents to the homebrew package install script

--- scripts/install/install-homebrew-packages.sh
+++ scripts/install/install-homebrew-packages.sh
@@ -4,14 +4,31 @@ echo "===== INSTALL HOMEBREW PACKAGES ====="
 
 ROOTDIR=$(cd "$(dirname "$0")/.." && pwd)
 
+brew tap homebrew/bundle
+brew tap homebrew/cask
+brew tap homebrew/cask-fonts
+brew tap homebrew/core
+brew tap homebrew/services
+
 # Install required tools and programs
 brew install \
 	caddy \
+	dash \
+	diction \
+	font-go \
+	font-go-mono \
+	git \
+	gnupg \
 	go \
 	gotop \
 	mtr \
 	nmap \
+	node \
+	postgresql \
+	rbenv-bundler \
 	ripgrep \
+	ruby \
+	thefuck \
 	tmux \
 	tree \
 	weechat \
